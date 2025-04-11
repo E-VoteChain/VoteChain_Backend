cfea5e7faa06fdfa04eb37f5f9da40635fdd5b50
shishiro <bannu1302@gmail.com>
2025-04-11 21:05:24 +0530

    remove undefined variables

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.put('/approve', verify_token, isAdmin, approve_user);
 router.put('/reject',verify_token, isAdmin, reject_user);
 router.get('/users', verify_token, isAdmin, get_users);
-router.post('/create-election', verify_token, isAdmin, create_election);
+// router.post('/create-election', verify_token, isAdmin, create_election);
 
 export default router;
